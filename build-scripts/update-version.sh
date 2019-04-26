9afbcbc84c4a393dd2dcf5ac57240079528d4d77
pmb <philbriones@qcsoftware.com>
2019-04-26 11:05:26 -0400

    java8 initial commit

---
+++ build-scripts/update-version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+updateMinor(){
+  VERSION_ARRAY[2]=0
+  ((VERSION_ARRAY[1]++ ))
+  LIB_VERSION=$(IFS="." ; echo "${VERSION_ARRAY[*]}")
+}
+
+updatePatch(){
+  ((VERSION_ARRAY[2]++ ))
+  LIB_VERSION=$(IFS="." ; echo "${VERSION_ARRAY[*]}")
+}
+LIB_VERSION=$(grep -oP '(?<=^version=).*' gradle.properties)
+IFS="." read -r -a VERSION_ARRAY<<<"$LIB_VERSION"
+LATEST_LOG=$(git log --oneline --decorate -1)
+if [[ "$LATEST_LOG" =~ feature/ ]]; then
+  updateMinor
+else
+  updatePatch
+fi
+sed -i "s/^\(version=\).*$/\1$LIB_VERSION/" gradle.properties
+echo "$LIB_VERSION"
