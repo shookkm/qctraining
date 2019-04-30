34c355334b52f10e28415f0c30c8f5a57a083624
Phillipe Briones <philbriones@qcsoftware.com>
2019-04-30 18:04:46 +0000

    Merged in remove-trailing-returns (pull request #5)
    remove trailing returns

--- build-scripts/update-version.sh
+++ build-scripts/update-version.sh
@@ -11,6 +11,8 @@ updatePatch(){
   LIB_VERSION=$(IFS="." ; echo "${VERSION_ARRAY[*]}")
 }
 LIB_VERSION=$(grep -oP '(?<=^version = ).*' gradle.properties)
+LIB_VERSION="$(echo -e "${LIB_VERSION}" | tr -d '[:space:]')"
+LIB_VERSION="$(echo -e "${LIB_VERSION}" | tr -d '\r')"
 IFS="." read -r -a VERSION_ARRAY<<<"$LIB_VERSION"
 LATEST_LOG=$(git log --oneline --decorate -1)
 if [[ "$LATEST_LOG" =~ feature/ ]]; then
